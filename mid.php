cad6b310e58c96f888ac475dbd1c99ac1cdb194b
Jozi Coates <105253374+jozicoates@users.noreply.github.com>
2022-09-28 22:34:07 -0400

    Update mid.php
    final

--- mid.php
+++ mid.php
@@ -10,6 +10,7 @@ $defaults = array(
     CURLOPT_URL => 'https://afsaccess4.njit.edu/~gc348/CS490/backend/users/get_user.php',
     CURLOPT_POST => true, 
     CURLOPT_POSTFIELDS => $data,
+    CURLOPT_RETURNTRANSFER => true
 );
 
 $ch = curl_init();  //initialize curl session
@@ -17,27 +18,23 @@ curl_setopt_array($ch, $defaults);
 
 
 //decode json from db.php
-$response = json_decode(curl_exec($ch));
+$response = json_decode(curl_exec($ch), true);
+$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
 
-//if username doesn't exist, echo 'invalid creds'
-if ($response == "User not found."){ //check syntax
-    echo "Invalid credentials."
-}
-
-
-//unhash password and compare to password gotten from post request
+//unhash password and compare to password gotten from post request, make sure username exists
 //json to index.php
-if (password_verify($response->{'password'}, $frontpassword)){ //check syntax
-    echo json_encode($role); //send role (teacher/student) to front
+if (password_verify($frontpassword, $response['password']) && $status == 200){ //check syntax
+    $returnarr = array(
+      "ucid" => $response["ucid"],
+      "role" => $response["role"]
+    );
+    echo json_encode($returnarr); //send role (teacher/student) to front
 }
 else{
-    http_response_code(404);
+    http_response_code(401);
     echo "Invalid credentials.";
     die();
 }
-
-
-
 ?>
